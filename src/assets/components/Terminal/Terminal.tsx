5a4370647da229bc379c4db38fb97385ee6047cd
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-09 14:53:17 -0600

    Remove unused CSS and TypeScript files

--- src/assets/components/Terminal/Terminal.tsx
+++
@@ -1,8 +0,0 @@
-import { Handle, HandleProps } from "@xyflow/react";
-import styles from "./styles.module.css";
-
-export function Terminal(props: HandleProps) {
-    return (
-        <Handle {...props} className={styles.terminal} />
-    );
-};
