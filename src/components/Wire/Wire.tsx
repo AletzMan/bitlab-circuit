280e6038e7a45522e35a07c22d7da61294e7e00a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-09 14:54:31 -0600

    feat: Add Wire component for connecting electrical components in the circuit visualization

---
+++ src/components/Wire/Wire.tsx
@@ -0,0 +1,17 @@
+import { BaseEdge, EdgeProps, getSmoothStepPath } from "@xyflow/react";
+
+
+export function Wire({ sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, markerEnd }: EdgeProps) {
+    const [d] = getSmoothStepPath({
+        sourceX,
+        sourceY,
+        targetX,
+        targetY,
+        sourcePosition,
+        targetPosition
+    });
+
+    return (
+        <BaseEdge path={d} style={{ stroke: "#00aaff", strokeWidth: 2 }} markerEnd={markerEnd} />
+    );
+};
