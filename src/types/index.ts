248cd9dc2f7e2eef81edae95522a9aeaa5a702b7
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-09 23:25:32 -0600

    feat: Add BoardIcon and ElectricalComponentState enum for displaying a board icon and managing component state

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,10 +2,19 @@ export enum ElectricalComponentType {
     Resistor = 'resistor',
     Capacitor = 'capacitor',
     Led = "led",
-    Battery = 'battery'
+    Battery = 'battery',
+    Board = 'board'
+}
+
+export enum ElectricalComponentState {
+    Add = 'add',
+    NotAdd = 'NnotAdd'
+
 }
 
 export type ElectricalComponentData = {
     value?: number
     type?: ElectricalComponentType
+    rotation?: number
+    state?: ElectricalComponentState
 }
