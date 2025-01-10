f882bb0e7962108c36359e4eac7796b8ededb50f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-09 23:25:10 -0600

    feat: Improve rotation handling for ElectricalComponent

--- src/components/ElectricalComponents/ElectricalComponent.tsx
+++ src/components/ElectricalComponents/ElectricalComponent.tsx
@@ -1,17 +1,27 @@
 import { Node, NodeProps, Position } from "@xyflow/react";
-import { ElectricalComponentData, ElectricalComponentType } from "@/types";
+import { ElectricalComponentData, ElectricalComponentState, ElectricalComponentType } from "@/types";
 import { CapacitorIcon, ResistorIcon } from "@/icons";
 import styles from "./styles.module.css";
 import { Terminal } from "@/components/Terminal/Terminal";
+import RotationHandle from "../RotationHandle/RotationHandle";
 
 type ElectricalComponentNode = Node<ElectricalComponentData, 'string'>;
 
-export function ElectricalComponent({ data: { type, value } }: NodeProps<ElectricalComponentNode>) {
+export function ElectricalComponent({ data: { type, value, rotation, state }, selected, id }: NodeProps<ElectricalComponentNode>) {
+
+    const isAdditionValid = state === ElectricalComponentState.Add;
+    const isAdditionInvalid = state === ElectricalComponentState.NotAdd;
+
     return (
-        <div className={styles.box}>
+        <div className={styles.box} style={{
+            transform: `rotate(${rotation}deg)`,
+            ...(isAdditionValid && { background: "#58ed58" }),
+            ...(isAdditionInvalid && { background: "#ff0505" }),
+        }}>
+            <RotationHandle selected={selected} id={id} />
             {type === ElectricalComponentType.Resistor && <ResistorIcon />}
             {type === ElectricalComponentType.Capacitor && <CapacitorIcon />}
-            <span className={styles.value}>{value}</span>
+            <span className={styles.value} style={{ transform: `rotate(-${rotation}deg)` }}>{value}</span>
             <Terminal type="source" position={Position.Right} id="right" />
             <Terminal type="source" position={Position.Left} id="left" />
         </div>
