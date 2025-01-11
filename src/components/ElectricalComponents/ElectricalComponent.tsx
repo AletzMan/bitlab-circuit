72492042386951cfbfab064024e9e80837c1c4a9
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-11 15:28:20 -0600

    refactor: Update ElectricalComponent to use ComponentNode and ComponentState

--- src/components/ElectricalComponents/ElectricalComponent.tsx
+++ src/components/ElectricalComponents/ElectricalComponent.tsx
@@ -1,17 +1,17 @@
 import { NodeProps, Position, useReactFlow } from "@xyflow/react";
-import { ElectricalComponentNode, ElectricalComponentState, ElectricalComponentType } from "@/types";
+import { ComponentNode, ComponentState, ComponentType } from "@/types";
 import { CapacitorIcon, LockIcon, ResistorIcon, UnlockIcon } from "@/icons";
 import styles from "./styles.module.css";
 import { Terminal } from "@/components/Terminal/Terminal";
 
 
 
-export function ElectricalComponent({ data: { type, value, rotation, state, isLock, prefix }, selected, id, parentId }: NodeProps<ElectricalComponentNode>) {
+export function ElectricalComponent({ data: { type, value, rotation, state, isLock, prefix }, selected, id, parentId }: NodeProps<ComponentNode>) {
     const { updateNode } = useReactFlow();
 
 
-    const isAdditionValid = state === ElectricalComponentState.Add;
-    const isAdditionInvalid = state === ElectricalComponentState.NotAdd;
+    const isAdditionValid = state === ComponentState.Add;
+    const isAdditionInvalid = state === ComponentState.NotAdd;
 
     let positionTerminals: Position[] = [Position.Right, Position.Left];
     if (rotation === 90) positionTerminals = [Position.Bottom, Position.Top];
@@ -21,16 +21,16 @@ export function ElectricalComponent({ data: { type, value, rotation, state, isLo
     return (
         <div className={`${styles.box} ${selected && styles.box_selected} ${isAdditionValid && styles.box_valid} ${isAdditionInvalid && styles.box_invalid}`} >
             {parentId && selected &&
-                <div className={styles.lock} onClick={() => updateNode(id, (prevNode) => ({ extent: prevNode.extent === 'parent' ? undefined : 'parent', data: { ...prevNode.data, isLock: !isLock } }))}>
-                    {!isLock && <UnlockIcon style={{ transform: `rotate(-${rotation}deg)` }} />}
-                    {isLock && <LockIcon style={{ transform: `rotate(-${rotation}deg)` }} />}
+                <div className={`${styles.lock} ${rotation === 90 && styles.lock_90}   ${rotation === 270 && styles.lock_270}`} onClick={() => updateNode(id, (prevNode) => ({ extent: prevNode.extent === 'parent' ? undefined : 'parent', data: { ...prevNode.data, isLock: !isLock } }))}>
+                    {!isLock && <UnlockIcon />}
+                    {isLock && <LockIcon />}
                 </div>
             }
             <div style={{ transform: `rotate(${rotation}deg)` }} className={styles.icon}>
-                {type === ElectricalComponentType.Resistor && <ResistorIcon />}
-                {type === ElectricalComponentType.Capacitor && <CapacitorIcon />}
+                {type === ComponentType.Resistor && <ResistorIcon />}
+                {type === ComponentType.Capacitor && <CapacitorIcon />}
             </div>
-            <span className={styles.value} style={{ transform: `rotate(${rotation - rotation}deg)` }}>{value}{prefix}</span>
+            <span className={`${styles.value} ${rotation === 90 && styles.value_90}   ${rotation === 270 && styles.value_270}`} style={{ transform: `rotate(${rotation - rotation}deg)` }}>{value}{prefix}</span>
             <Terminal type="source" position={positionTerminals[0]} id="1" />
             <Terminal type="source" position={positionTerminals[1]} id="2" />
         </div>
