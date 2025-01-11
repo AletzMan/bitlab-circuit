a64444617f009cf15ec9cc17c33a7533355a2bc3
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-11 00:32:12 -0600

    refactor: Optimize Terminal component for better performance

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,6 +1,8 @@
 import { Handle, HandleProps } from "@xyflow/react";
 import styles from "./styles.module.css";
 
+
+
 export function Terminal(props: HandleProps) {
     return (
         <Handle {...props} className={styles.terminal} />
