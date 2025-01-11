3b16d4c0ec44eb634231f87c58917c83a12eaf69
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-11 00:21:59 -0600

    feat: Add flip and rotate functionality to ComponentDetails

--- src/components/ComponentDetails/ComponentDetails.tsx
+++ src/components/ComponentDetails/ComponentDetails.tsx
@@ -1,19 +1,11 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useReactFlow } from "@xyflow/react";
+import { useReactFlow, useUpdateNodeInternals } from "@xyflow/react";
 import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import { FlipHIcon, FlipVIcon, RotateLeftIcon, RotateRightIcon } from "@/icons";
 import { Input, Select, Button, Flex, Divider, Card } from "antd";
-import { ElectricalComponentNode } from "@/types";
+import { ElectricalComponentData, ElectricalComponentNode, UNITS } from "@/types";
 const { Option } = Select;
 
-const selectAfter = (
-    <Select defaultValue="Ω">
-        <Option value="Ω">Ω</Option>
-        <Option value="KΩ">KΩ</Option>
-        <Option value="MΩ">MΩ</Option>
-    </Select>
-);
 
 export default function ComponentDetail({
     node,
@@ -21,11 +13,12 @@ export default function ComponentDetail({
     node: ElectricalComponentNode | undefined;
 }) {
     const nodeType = node?.data?.type || node?.type;
-    const [value, setValue] = useState<string | undefined>(`${node?.data?.value}`);
+    const [dataComponent, setDataComponent] = useState<ElectricalComponentData | undefined>();
+    const updateNodeInternals = useUpdateNodeInternals();
 
     useEffect(() => {
-        setValue(node?.data.value?.toString());
-    }, [node]);
+        setDataComponent(node?.data);
+    }, [node, node?.data]);
 
 
     const { updateNodeData } = useReactFlow();
@@ -34,54 +27,69 @@ export default function ComponentDetail({
 
     const handleFlipHorizontal = () => {
         const newRotation = node?.data.rotation === 90 ? 270 : 90;
-        if (node) {
-            updateNodeData(node?.id, { rotation: newRotation });
+        if (node && dataComponent) {
+            setDataComponent({ ...dataComponent, rotation: newRotation });
+            updateNodeData(node.id, { rotation: newRotation });
+            updateNodeInternals(node.id);
         }
     };
 
     const handleFlipVertical = () => {
         const newRotation = node?.data.rotation === 0 ? 180 : 0;
-        if (node) {
-            updateNodeData(node?.id, { rotation: newRotation });
+        if (node && dataComponent) {
+            setDataComponent({ ...dataComponent, rotation: newRotation });
+            updateNodeData(node.id, { rotation: newRotation });
+            updateNodeInternals(node.id);
         }
     };
 
     const handleRotateLeft = () => {
-        if (node) {
-            const checkRoation = node?.data?.rotation === 0 ? 360 : node?.data?.rotation;
+        if (node && dataComponent) {
+            const checkRoation = dataComponent.rotation === 0 ? 360 : dataComponent.rotation;
             let newRotation = checkRoation - 90;
             newRotation = newRotation === 360 ? 0 : newRotation;
-            updateNodeData(node?.id, { rotation: newRotation });
+            setDataComponent({ ...dataComponent, rotation: newRotation });
+            updateNodeData(node.id, { rotation: newRotation });
+            updateNodeInternals(node.id);
         }
     };
 
     const handleRotateRight = () => {
-        if (node) {
-            let newRotation = node.data.rotation + 90;
+        if (node && dataComponent) {
+            let newRotation = dataComponent.rotation + 90;
             newRotation = newRotation === 360 ? 0 : newRotation;
-            if (node) {
-                updateNodeData(node.id, { rotation: newRotation });
-            }
+            setDataComponent({ ...dataComponent, rotation: newRotation });
+            updateNodeData(node.id, { rotation: newRotation });
+            updateNodeInternals(node.id);
         }
     };
 
-    console.log(node);
+    const handleChangeUnit = (value: string) => {
+        console.log(value);
+    };
 
-    useEffect(() => {
-    }, [node?.data]);
     return (
         <Card className={styles.details} title="Details" size="small">
             <div className={styles.details_name}  >{nodeType?.toUpperCase()}</div>
             {node?.data?.value && (
                 <Input
                     className={styles.value_number}
-                    value={value}
+                    value={dataComponent?.value}
                     onChange={(e) => {
                         const newValue = e.target.value ? parseFloat(e.target.value) : 0;
-                        setValue(newValue.toString());
-                        updateNodeData(node.id, { value: newValue });
+                        if (dataComponent) {
+                            setDataComponent({ ...dataComponent, value: newValue });
+                            updateNodeData(node.id, { value: newValue });
+                        }
                     }}
-                    addonAfter={selectAfter}
+                    addonAfter={dataComponent &&
+                        <Select defaultValue={dataComponent.prefix} value={dataComponent.prefix} onChange={handleChangeUnit}>
+                            {UNITS[dataComponent.unit].map(value => (
+                                <Option key={value} value={value}>{value}</Option>
+                            )
+
+                            )}
+                        </Select>}
                 />
             )}
             <Divider />
