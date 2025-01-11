e364d0324a6ba0039fb77bbace38cb94cedf6314
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-11 16:03:58 -0600

    refactor: Update AnalogComponent to use ComponentNode and ComponentState

---
+++ src/components/AnalogComponent/AnalogComponent.tsx
@@ -0,0 +1,38 @@
+import { NodeProps, Position, useReactFlow } from "@xyflow/react";
+import { ComponentNode, ComponentState, ComponentType } from "@/types";
+import { CapacitorIcon, LockIcon, ResistorIcon, UnlockIcon } from "@/icons";
+import styles from "./styles.module.css";
+import { Terminal } from "@/components/Terminal/Terminal";
+
+
+
+export function AnalogComponent({ data: { type, value, rotation, state, isLock, prefix }, selected, id, parentId }: NodeProps<ComponentNode>) {
+    const { updateNode } = useReactFlow();
+
+
+    const isAdditionValid = state === ComponentState.Add;
+    const isAdditionInvalid = state === ComponentState.NotAdd;
+
+    let positionTerminals: Position[] = [Position.Right, Position.Left];
+    if (rotation === 90) positionTerminals = [Position.Bottom, Position.Top];
+    if (rotation === 180) positionTerminals = [Position.Left, Position.Right];
+    if (rotation === 270) positionTerminals = [Position.Top, Position.Bottom];
+
+    return (
+        <div className={`${styles.box} ${selected && styles.box_selected} ${isAdditionValid && styles.box_valid} ${isAdditionInvalid && styles.box_invalid}`} >
+            {parentId && selected &&
+                <div className={`${styles.lock} ${rotation === 90 && styles.lock_90}   ${rotation === 270 && styles.lock_270}`} onClick={() => updateNode(id, (prevNode) => ({ extent: prevNode.extent === 'parent' ? undefined : 'parent', data: { ...prevNode.data, isLock: !isLock } }))}>
+                    {!isLock && <UnlockIcon />}
+                    {isLock && <LockIcon />}
+                </div>
+            }
+            <div style={{ transform: `rotate(${rotation}deg)` }} className={styles.icon}>
+                {type === ComponentType.Resistor && <ResistorIcon />}
+                {type === ComponentType.Capacitor && <CapacitorIcon />}
+            </div>
+            <span className={`${styles.value} ${rotation === 90 && styles.value_90}   ${rotation === 270 && styles.value_270}`} style={{ transform: `rotate(${rotation - rotation}deg)` }}>{value}{prefix}</span>
+            <Terminal type="source" position={positionTerminals[0]} id="1" />
+            <Terminal type="source" position={positionTerminals[1]} id="2" />
+        </div>
+    );
+}
