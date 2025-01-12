6c3b51bb1fd416b4d5537a15fb38f74a7da73f1f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-12 03:13:35 -0600

    refactor: Add UndoIcon and RedoIcon components to icons/index.tsx

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -119,3 +119,19 @@ export function ResetZoomIcon(props: SVGProps<SVGSVGElement>) {
         </svg>
     );
 }
+
+export function UndoIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg {...props} width="2em" height="2em" viewBox="0 0 24 24">
+            <path fill="currentColor" fillRule="evenodd" d="M7.53 3.47a.75.75 0 0 1 0 1.06L5.81 6.25H15a5.75 5.75 0 0 1 0 11.5H8a.75.75 0 0 1 0-1.5h7a4.25 4.25 0 0 0 0-8.5H5.81l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3a.75.75 0 0 1 1.06 0" clipRule="evenodd"></path>
+        </svg>
+    );
+}
+
+export function RedoIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg {...props} width="2em" height="2em" viewBox="0 0 24 24" style={{ transform: "scaleX(-1)" }}>
+            <path fill="currentColor" fillRule="evenodd" d="M7.53 3.47a.75.75 0 0 1 0 1.06L5.81 6.25H15a5.75 5.75 0 0 1 0 11.5H8a.75.75 0 0 1 0-1.5h7a4.25 4.25 0 0 0 0-8.5H5.81l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3a.75.75 0 0 1 1.06 0" clipRule="evenodd"></path>
+        </svg>
+    );
+}
