23a54a362fe617b2e803660fd3a1568104d854c8
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-14 04:03:31 -0600

    refactor: Update EdgeDetails component to handle color changes for wires

--- src/components/EdgeDetails/EdgeDetails.tsx
+++ src/components/EdgeDetails/EdgeDetails.tsx
@@ -1,8 +1,9 @@
-import { Edge } from "@xyflow/react";
+
 import styles from "./styles.module.css";
 import { DeletetIcon } from "@/icons";
 import { Button, Flex, Card, Divider } from "antd";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { ComponentEdge } from "@/types";
 
 
 export default function EdgeDetails({
@@ -11,15 +12,17 @@ export default function EdgeDetails({
     removeEdges,
     isSingleEdgeSelection,
     selectedEdges,
+    setEdges,
 }: {
-    edge: Edge;
-    setSelectedEdge: Dispatch<SetStateAction<Edge | undefined>>,
-    removeEdges: (edges: Edge[] | undefined) => void,
+    edge: ComponentEdge;
+    setSelectedEdge: Dispatch<SetStateAction<ComponentEdge | undefined>>,
+    removeEdges: (edges: ComponentEdge[] | undefined) => void,
     isSingleEdgeSelection: boolean,
-    selectedEdges: Edge[]
+    selectedEdges: ComponentEdge[],
+    setEdges: React.Dispatch<React.SetStateAction<ComponentEdge[]>>
 }) {
 
-
+    const [currentColor, setCurrentColor] = useState(edge?.data?.color);
     const handleDelete = () => {
         if (isSingleEdgeSelection) {
             removeEdges([edge]);
@@ -29,10 +32,23 @@ export default function EdgeDetails({
         setSelectedEdge(undefined);
     };
 
+    const handleChangeColorWire = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newColor = e.currentTarget.value;
+        setEdges((prevEdges) =>
+            prevEdges.map((edgePrev) =>
+                edgePrev.id === edge.id
+                    ? { ...edgePrev, data: { ...edgePrev.data, color: newColor, path: "" } }
+                    : edgePrev
+            )
+        );
+        setCurrentColor(newColor);
+    };
+
     return (
         <Card className={styles.details} size="small" type="inner" >
             <Flex vertical>
                 <label className="details_name"  >Wire</label>
+                <input type="color" value={currentColor} onChange={handleChangeColorWire} />
                 <Divider style={{ margin: "16px 0" }} />
                 <label className="label">Actions</label>
                 <Flex gap={10} wrap>
