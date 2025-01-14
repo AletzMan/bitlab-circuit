9a76b54872c1f55b6f4147684071910197e7b6e6
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-14 04:20:21 -0600

    refactor: Update Wire component to use dynamic stroke color

--- src/components/Wire/Wire.tsx
+++ src/components/Wire/Wire.tsx
@@ -2,7 +2,7 @@ import { BaseEdge, EdgeProps, getSmoothStepPath } from "@xyflow/react";
 import styles from "./styles.module.css";
 import { ComponentEdge } from "@/types";
 
-export function Wire({ sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition }: EdgeProps<ComponentEdge>) {
+export function Wire({ sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, data }: EdgeProps<ComponentEdge>) {
     const [d] = getSmoothStepPath({
         sourceX,
         sourceY,
@@ -14,6 +14,6 @@ export function Wire({ sourceX, sourceY, targetX, targetY, sourcePosition, targe
     });
 
     return (
-        <BaseEdge path={d} className={styles.wire} type="smoothstep" />
+        <BaseEdge path={d} className={styles.wire} style={{ stroke: data?.color }} type="smoothstep" />
     );
 };
