56d0c0be5171f6af7a833a618675ccd804599f3d
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-15 00:03:52 -0600

    refactor: Update CustomColorsWire in constants/index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -25,16 +25,28 @@ export const ARRAY_COMPONENTS: ComponentType[] = [
 ];
 
 export const CustomColorsWire = {
-    customPalette: [
+    lightPalette: [
+        '#000000', // Negro
         '#FF5733', // Rojo brillante
+        '#FF8D33', // Naranja
         '#FFC300', // Amarillo
-        '#DAF7A6', // Verde claro
+        '#DAF7A6', // Verde lima
         '#33FF57', // Verde
-        '#33FFBD', // Verde azulado
+        '#33FFBD', // Cian
         '#33C4FF', // Azul claro
-        '#337BFF', // Azul oscuro
-        '#8C33FF', // Morado
-        '#FF33A1', // Rosa
-        '#FF3358', // Rojo intenso
+        '#337BFF', // Azul medio
+        '#8C33FF', // Violeta 
+    ],
+    darkPalette: [
+        '#FFFFFF', // Blanco
+        '#FF6F61', // Coral
+        '#FF914D', // Naranja oscuro
+        '#FFC300', // Oro
+        '#B8E986', // Verde amarillento
+        '#33FF88', // Verde lima
+        '#33D1FF', // Azul acero
+        '#337BFF', // Azul medio
+        '#A569BD', // Púrpura medio
+        '#6C3483', // Púrpura oscuro 
     ],
 };
