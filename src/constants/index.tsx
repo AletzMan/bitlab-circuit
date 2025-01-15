50616f54bf9c6a3cd357f043b2a091d0195af5d7
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-14 23:30:49 -0600

    refactor: Update CustomColorsWire in constants/index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -23,3 +23,18 @@ export const ARRAY_COMPONENTS: ComponentType[] = [
     ComponentType.Resistor,
     ComponentType.Capacitor,
 ];
+
+export const CustomColorsWire = {
+    customPalette: [
+        '#FF5733', // Rojo brillante
+        '#FFC300', // Amarillo
+        '#DAF7A6', // Verde claro
+        '#33FF57', // Verde
+        '#33FFBD', // Verde azulado
+        '#33C4FF', // Azul claro
+        '#337BFF', // Azul oscuro
+        '#8C33FF', // Morado
+        '#FF33A1', // Rosa
+        '#FF3358', // Rojo intenso
+    ],
+};
