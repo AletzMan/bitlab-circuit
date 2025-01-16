cd6f2d23824e0aa66fa9d62ff3bdaee973842763
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-16 00:15:06 -0600

    Update: add logic for limit connection by terminal

--- src/components/AnalogComponent/AnalogComponent.tsx
+++ src/components/AnalogComponent/AnalogComponent.tsx
@@ -1,16 +1,45 @@
-import { NodeProps, Position, useReactFlow } from "@xyflow/react";
+import { Connection, NodeProps, Position, useNodeConnections, useReactFlow } from "@xyflow/react";
 import { AnalogNode, ComponentState, ComponentType } from "@/types";
 import { CapacitorElectrolyticIcon, CapacitorIcon, DiodeIcon, InductorIcon, LockIcon, ResistorIcon, UnlockIcon } from "@/icons";
 import styles from "./styles.module.css";
 import { Terminal } from "@/components/Terminal/Terminal";
+import { useState } from "react";
 
 
 export function AnalogComponent({ data: { type, value, rotation, state, isLock, prefix, reference }, selected, id, parentId }: NodeProps<AnalogNode>) {
     const { updateNode } = useReactFlow();
-
+    const [isConnected, setIsConnected] = useState<boolean[]>([false, false]);
 
     const isAdditionValid = state === ComponentState.Add;
     const isAdditionInvalid = state === ComponentState.NotAdd;
+
+    const onConnect = (connections: Connection[]) => {
+        setConnectionsTerminals(connections, true);
+    };
+
+    const onDisconnect = (connections: Connection[]) => {
+        setConnectionsTerminals(connections, false);
+    };
+
+    useNodeConnections({ onConnect, onDisconnect });
+
+    const setConnectionsTerminals = (connections: Connection[], isOnConnect: boolean) => {
+        connections.map((connection) => {
+            const newState = [...isConnected];
+            if (connection.target === id) {
+                const handleNumber = Number(connection.targetHandle) - 1;
+                newState[handleNumber] = isOnConnect;
+                setIsConnected(newState);
+                return connection.target === id;
+            }
+            if (connection.source === id) {
+                const handleNumber = Number(connection.sourceHandle) - 1;
+                newState[handleNumber] = isOnConnect;
+                setIsConnected(newState);
+                return connection.source === id;
+            }
+        });
+    };
 
     let positionTerminals: Position[] = [Position.Right, Position.Left];
     if (rotation === 90) positionTerminals = [Position.Bottom, Position.Top];
@@ -36,8 +65,8 @@ export function AnalogComponent({ data: { type, value, rotation, state, isLock, 
             </div>
             <span className={`${styles.value} ${rotation === 90 && styles.value_90}   ${rotation === 270 && styles.value_270}`} style={{ transform: `rotate(${rotation - rotation}deg)` }}>{value}{prefix}</span>
             <span className={`${styles.id} ${rotation === 90 && styles.value_90}   ${rotation === 270 && styles.value_270}`} style={{ transform: `rotate(${rotation - rotation}deg)` }}>{reference}</span>
-            <Terminal type="source" position={positionTerminals[0]} id="1" />
-            <Terminal type="source" position={positionTerminals[1]} id="2" />
+            <Terminal type="source" position={positionTerminals[0]} id="1" isConnectable={!isConnected[0]} />
+            <Terminal type="source" position={positionTerminals[1]} id="2" isConnectable={!isConnected[1]} />
         </div>
     );
 }
