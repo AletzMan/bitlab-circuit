abfb470d11c3c0281c48f9add45625749589f51f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-15 18:30:40 -0600

    Update: change svg od node icon

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,9 +1,12 @@
+
 import { Handle, HandleProps } from "@xyflow/react";
 import styles from "./styles.module.css";
 
+export interface HandlePropsNew extends HandleProps {
+    idNode?: string
+}
 
-
-export function Terminal(props: HandleProps) {
+export function Terminal(props: HandlePropsNew) {
     return (
         <Handle {...props} className={styles.terminal} />
     );
