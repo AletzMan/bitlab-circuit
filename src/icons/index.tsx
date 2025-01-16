5170afe5e3b070ecddb3523720e25cf43d52fbb9
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-16 01:35:25 -0600

    style: change color icon sun

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -272,7 +272,7 @@ export function DarkIcon(props: SVGProps<SVGSVGElement>) {
 export function LightIcon(props: SVGProps<SVGSVGElement>) {
     return (
         <svg {...props} width="1.75em" height="1.75em" viewBox="0 0 24 24" style={{ transform: "translateY(-4px)" }}>
-            <g fill="none" stroke="currentColor" strokeWidth={1.5}>
+            <g fill="none" stroke="#f0b62e" strokeWidth={1.5}>
                 <circle cx={12} cy={12} r={4} strokeLinejoin="round"></circle>
                 <path strokeLinecap="round" d="M20 12h1M3 12h1m8 8v1m0-18v1m5.657 13.657l.707.707M5.636 5.636l.707.707m0 11.314l-.707.707M18.364 5.636l-.707.707"></path>
             </g>
