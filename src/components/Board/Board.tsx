c6d0a33f3c696e5fa69c5d2b253ed69fb9b2efac
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-16 02:14:34 -0600

    Styles: change color board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,21 +1,17 @@
 
 
-import { Node, NodeProps, NodeResizer, useStore } from "@xyflow/react";
+import { Node, NodeProps, NodeResizer } from "@xyflow/react";
 import { ComponentData } from "@/types";
-import { zoomSelector } from "@/helpers";
 import styles from "./styles.module.css";
 
 type BoardNode = Node<ComponentData, "string">;
 
 export function Board({ selected }: NodeProps<BoardNode>) {
-    const showContent = useStore(zoomSelector);
-
 
     return (
         <div className={`${styles.board} ${selected && styles.board_selected}`}
         >
             {selected && <NodeResizer minWidth={200} minHeight={200} />}
-            {!showContent && <span />}
         </div>
     );
 }
