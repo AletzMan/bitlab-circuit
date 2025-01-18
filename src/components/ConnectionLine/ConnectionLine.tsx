61667bb6a108590fe2bb9bd68be384b0cee73c82
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-17 22:05:11 -0600

    Update: change path connection

--- src/components/ConnectionLine/ConnectionLine.tsx
+++ src/components/ConnectionLine/ConnectionLine.tsx
@@ -1,13 +1,24 @@
-import { ConnectionLineComponentProps, getSimpleBezierPath } from "@xyflow/react";
+import { ConnectionLineComponentProps, getSmoothStepPath } from "@xyflow/react";
 
 
-export function ConnectionLine({ fromX, fromY, toX, toY, connectionStatus }: ConnectionLineComponentProps) {
-    const [d] = getSimpleBezierPath({ sourceX: fromX, sourceY: fromY, targetX: toX, targetY: toY });
-    let color = 'black';
+export function ConnectionLine({ fromX, fromY, toX, toY, fromPosition, toPosition, connectionStatus }: ConnectionLineComponentProps) {
+    const [d] = getSmoothStepPath({
+        sourceX: fromX,
+        sourceY: fromY,
+        targetX: toX,
+        targetY: toY,
+        sourcePosition: fromPosition,
+        targetPosition: toPosition,
+        borderRadius: 2, offset: 18
+
+    });
+
+
+    let color = 'white';
     if (connectionStatus === 'valid') color = '#2dd451';
     if (connectionStatus === 'invalid') color = '#f33524';
 
     return (
-        <path fill="none" stroke={color} strokeWidth={1.5} d={d} />
+        <path fill="none" stroke={color} strokeWidth={1.5} d={d} type="smoothstep" />
     );
 };
