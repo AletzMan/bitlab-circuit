25eac5df85b578ff872229700549e6e2ad8c8c26
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-22 15:35:09 -0600

    refactor: Update TransistorComponent styles and types

--- src/components/TransistorComponent/TransistorComponent.tsx
+++ src/components/TransistorComponent/TransistorComponent.tsx
@@ -3,7 +3,7 @@ import { AnalogNode, ComponentState, ComponentType } from "@/types";
 import { LEDIcon, LockIcon, UnlockIcon } from "@/icons";
 import styles from "./styles.module.css";
 import { Terminal } from "@/components/Terminal/Terminal";
-import { useEffect, useMemo, useState } from "react";
+import { CSSProperties, useEffect, useMemo, useState } from "react";
 import { COMPONENTS } from "@/constants";
 
 
@@ -49,63 +49,79 @@ export function TransistorComponent({ data: { type, value, rotation, flip, state
         });
     };
 
-    const positionTerminals: Position[] = useMemo(() => {
+    const positionTerminals: { position: Position[], adjustment: CSSProperties[] } = useMemo(() => {
         switch (rotation) {
             case 0: {
                 let tempRotation: Position[] = [Position.Left, Position.Top, Position.Bottom];
+                let adjustment: CSSProperties[] = [{ top: 'calc(50% + 6px)' }, { left: 'calc(50% + 5px)' }, { left: 'calc(50% + 5px)' }];
                 if (flip.x === -1 && flip.y === 1) {
                     tempRotation = [Position.Right, Position.Left, Position.Top];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === 1) {
                     tempRotation = [Position.Left, Position.Right, Position.Bottom];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === -1) {
                     tempRotation = [Position.Right, Position.Left, Position.Bottom];
+                    adjustment = [];
                 }
-                return tempRotation;
+                return { position: tempRotation, adjustment };
             }
             case 90: {
                 let tempRotation: Position[] = [Position.Top, Position.Right, Position.Left];
+                let adjustment: CSSProperties[] = [];
                 if (flip.x === -1 && flip.y === 1) {
                     tempRotation = [Position.Top, Position.Bottom, Position.Left];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === 1) {
                     tempRotation = [Position.Bottom, Position.Top, Position.Right];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === -1) {
                     tempRotation = [Position.Bottom, Position.Top, Position.Left];
+                    adjustment = [];
                 }
-                return tempRotation;
+                return { position: tempRotation, adjustment };
             }
             case 180: {
                 let tempRotation: Position[] = [Position.Right, Position.Left, Position.Bottom];
+                let adjustment: CSSProperties[] = [];
                 if (flip.x === -1 && flip.y === 1) {
                     tempRotation = [Position.Left, Position.Right, Position.Bottom];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === 1) {
                     tempRotation = [Position.Right, Position.Left, Position.Top];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === -1) {
                     tempRotation = [Position.Left, Position.Right, Position.Top];
+                    adjustment = [];
                 }
-                return tempRotation;
+                return { position: tempRotation, adjustment };
             }
             case 270: {
                 let tempRotation: Position[] = [Position.Bottom, Position.Top, Position.Left];
+                let adjustment: CSSProperties[] = [];
                 if (flip.x === -1 && flip.y === 1) {
                     tempRotation = [Position.Bottom, Position.Top, Position.Right];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === 1) {
                     tempRotation = [Position.Top, Position.Bottom, Position.Left];
+                    adjustment = [];
                 }
                 if (flip.y === -1 && flip.x === -1) {
                     tempRotation = [Position.Top, Position.Bottom, Position.Right];
+                    adjustment = [];
                 }
-                return tempRotation;
+                return { position: tempRotation, adjustment };
             }
 
             default:
-                return [Position.Right, Position.Left, Position.Top];
+                return { position: [Position.Right, Position.Left, Position.Top], adjustment: [] };
         }
     }, [rotation, flip.x, flip.y]);
 
@@ -123,9 +139,9 @@ export function TransistorComponent({ data: { type, value, rotation, flip, state
                     : COMPONENTS[type].icon
                 }
             </div>
-            <Terminal type="source" position={positionTerminals[0]} id="1" isConnectable={!isConnected[0]} style={{ top: positionTerminals[0] === Position.Left ? "calc(50% + 6px)" : '' }} />
-            <Terminal type="source" position={positionTerminals[1]} id="2" isConnectable={!isConnected[1]} />
-            {<Terminal type="source" position={positionTerminals[2]} id="3" isConnectable={!isConnected[2]} />}
+            <Terminal type="source" position={positionTerminals.position[0]} id="1" isConnectable={!isConnected[0]} style={positionTerminals.adjustment[0]} />
+            <Terminal type="source" position={positionTerminals.position[1]} id="2" isConnectable={!isConnected[1]} style={positionTerminals.adjustment[1]} />
+            {<Terminal type="source" position={positionTerminals.position[2]} id="3" isConnectable={!isConnected[2]} style={positionTerminals.adjustment[2]} />}
             {isValueVisible && <span className={`${styles.value}  ${size === 'small' && styles.value_small} ${size === 'medium' && styles.value_medium} ${size === 'large' && styles.value_large}  ${rotation === 90 && styles.value_90}   ${rotation === 270 && styles.value_270}`} style={{ transform: `rotate(${rotation - rotation}deg) ` }}>{value}{prefix}</span>}
             {isReferenceVisible && <span className={`${styles.reference} ${size === 'small' && styles.reference_small} ${size === 'medium' && styles.reference_medium} ${size === 'large' && styles.reference_large} ${rotation === 90 && styles.reference_90}   ${rotation === 270 && styles.reference_270}`} style={{ transform: `rotate(${rotation - rotation}deg)` }} >{reference}</span>}
         </div>
