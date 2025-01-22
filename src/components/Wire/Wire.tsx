48812a918537fad8c6adf0051c2bbc7b19ecda81
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-21 22:16:00 -0600

    refactor: Update Wire component styles to include marker end for arrows

--- src/components/Wire/Wire.tsx
+++ src/components/Wire/Wire.tsx
@@ -1,4 +1,4 @@
-import { BaseEdge, EdgeProps, getSmoothStepPath } from "@xyflow/react";
+import { BaseEdge, EdgeProps, MarkerType, getSmoothStepPath } from "@xyflow/react";
 import styles from "./styles.module.css";
 import { ComponentEdge } from "@/types";
 import { CSSProperties } from "react";
@@ -19,6 +19,6 @@ export function Wire({ sourceX, sourceY, targetX, targetY, sourcePosition, targe
 
 
     return (
-        <BaseEdge interactionWidth={5} path={d} className={styles.wire} style={{ "--wire-color": ((data?.color === "#000000" || data?.color === "rgb(0,0,0)") && currentTheme === 'dark') ? '#FFFFFF' : ((data?.color.toLowerCase() === "#ffffff" || data?.color === "rgb(255,255,255)") && currentTheme === 'light') ? "#000000" : data?.color } as CSSProperties} type="smoothstep" />
+        <BaseEdge interactionWidth={5} markerEnd={MarkerType.Arrow} path={d} className={styles.wire} style={{ "--wire-color": ((data?.color === "#000000" || data?.color === "rgb(0,0,0)") && currentTheme === 'dark') ? '#FFFFFF' : ((data?.color.toLowerCase() === "#ffffff" || data?.color === "rgb(255,255,255)") && currentTheme === 'light') ? "#000000" : data?.color } as CSSProperties} type="smoothstep" />
     );
 };
