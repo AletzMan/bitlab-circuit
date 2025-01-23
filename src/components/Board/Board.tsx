b3938bf310fbfd98d29ff6d6368ba84cdaf0f6c5
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-22 18:30:57 -0600

    refactor: Add label to Board component when reference is visible

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,11 +6,12 @@ import styles from "./styles.module.css";
 
 type BoardNode = Node<ComponentData, "string">;
 
-export function Board({ selected }: NodeProps<BoardNode>) {
+export function Board({ selected, data: { isReferenceVisible, reference } }: NodeProps<BoardNode>) {
 
     return (
         <div className={`${styles.board} ${selected && styles.board_selected}`}
         >
+            {isReferenceVisible && <label className={styles.board_label}>{reference}</label>}
             {selected && <NodeResizer minWidth={200} minHeight={200} />}
         </div>
     );
