28e9084f35cdd90e0aa8dfa1219ed3b3b295ab1a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-23 17:49:18 -0600

    refactor: Add AnalogNode and ComponentEdge types to store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 
 
+import { AnalogNode, ComponentEdge } from "@/types";
 import { theme } from "antd";
 import { } from "antd/lib/theme/interface/maps";
 import { create, } from "zustand";
@@ -28,3 +29,52 @@ export const useTheme = create(
 );
 
 
+interface INodeSelected {
+    selectedNode: AnalogNode | undefined
+    setSelectedNode: (value: AnalogNode | undefined) => void
+    selectedNodes: AnalogNode[] | undefined
+    setSelectedNodes: (value: AnalogNode[] | undefined) => void
+    selectedEdge: ComponentEdge | undefined
+    setSelectedEdge: (value: ComponentEdge | undefined) => void
+    selectedEdges: ComponentEdge[]
+    setSelectedEdges: (value: ComponentEdge[]) => void
+    isSingleNodeSelection: boolean
+    setIsSingleNodeSelection: (value: boolean) => void
+    isSingleEdgeSelection: boolean
+    setIsSingleEdgeSelection: (value: boolean) => void
+}
+
+export const useSelectedItems = create<INodeSelected>(
+    (set) => ({
+        selectedNode: undefined,
+        setSelectedNode: (value: AnalogNode | undefined) =>
+            set(() => ({
+                selectedNode: value,
+            })),
+        selectedNodes: undefined,
+        setSelectedNodes: (value: AnalogNode[] | undefined) =>
+            set(() => ({
+                selectedNodes: value,
+            })),
+        selectedEdge: undefined,
+        setSelectedEdge: (value: ComponentEdge | undefined) =>
+            set(() => ({
+                selectedEdge: value,
+            })),
+        selectedEdges: [],
+        setSelectedEdges: (value: ComponentEdge[]) =>
+            set(() => ({
+                selectedEdges: value,
+            })),
+        isSingleNodeSelection: false,
+        setIsSingleNodeSelection: (value: boolean) =>
+            set(() => ({
+                isSingleNodeSelection: value,
+            })),
+        isSingleEdgeSelection: false,
+        setIsSingleEdgeSelection: (value: boolean) =>
+            set(() => ({
+                isSingleEdgeSelection: value,
+            })),
+    })
+);
