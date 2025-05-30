2aa706ec6ba1557c3fc8bc2403df83c4abdf1e07
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-30 02:39:39 -0600

    refactor: Update App component to include ConfigProvider for theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-
-import { ReactFlowProvider } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
-import './App.css';
-import { BoardFlow } from '@/components/BoardFlow/BoardFlow';
-
+import { ReactFlowProvider } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import "./App.css";
+import { BoardFlow } from "@/components/BoardFlow/BoardFlow";
+import { ConfigProvider } from "antd";
+import theme from "./theme/themeConfig";
 
 function App() {
-
-
-  return (
-    <ReactFlowProvider>
-      <BoardFlow />
-    </ReactFlowProvider>
-  );
+	return (
+		<ConfigProvider theme={theme}>
+			<ReactFlowProvider>
+				<BoardFlow />
+			</ReactFlowProvider>
+		</ConfigProvider>
+	);
 }
 
 export default App;
