d68e2de69ed979b46db56ca3808b9a0aa61a7930
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-30 02:40:40 -0600

    style: Refactor useSelectedItemsState hook for improved readability and consistency

--- src/hooks/useSelectedItemsState.ts
+++ src/hooks/useSelectedItemsState.ts
@@ -1,29 +1,29 @@
 import { useSelectedItems } from "@/store";
 
 export function useSelectedItemsState() {
-    const selectedNode = useSelectedItems((state) => state.selectedNode);
-    const setSelectedNode = useSelectedItems((state) => state.setSelectedNode);
-    const selectedNodes = useSelectedItems((state) => state.selectedNodes);
-    const setSelectedNodes = useSelectedItems((state) => state.setSelectedNodes);
-    const selectedEdge = useSelectedItems((state) => state.selectedEdge);
-    const setSelectedEdge = useSelectedItems((state) => state.setSelectedEdge);
-    const selectedEdges = useSelectedItems((state) => state.selectedEdges);
-    const setSelectedEdges = useSelectedItems((state) => state.setSelectedEdges);
-    const isSingleEdgeSelection = useSelectedItems((state) => state.isSingleEdgeSelection);
-    const setIsSingleNodeSelection = useSelectedItems((state) => state.setIsSingleNodeSelection);
-    const setIsSingleEdgeSelection = useSelectedItems((state) => state.setIsSingleEdgeSelection);
+	const selectedNode = useSelectedItems((state) => state.selectedNode);
+	const setSelectedNode = useSelectedItems((state) => state.setSelectedNode);
+	const selectedNodes = useSelectedItems((state) => state.selectedNodes);
+	const setSelectedNodes = useSelectedItems((state) => state.setSelectedNodes);
+	const selectedEdge = useSelectedItems((state) => state.selectedEdge);
+	const setSelectedEdge = useSelectedItems((state) => state.setSelectedEdge);
+	const selectedEdges = useSelectedItems((state) => state.selectedEdges);
+	const setSelectedEdges = useSelectedItems((state) => state.setSelectedEdges);
+	const isSingleEdgeSelection = useSelectedItems((state) => state.isSingleEdgeSelection);
+	const setIsSingleNodeSelection = useSelectedItems((state) => state.setIsSingleNodeSelection);
+	const setIsSingleEdgeSelection = useSelectedItems((state) => state.setIsSingleEdgeSelection);
 
-    return {
-        selectedNode,
-        setSelectedNode,
-        selectedNodes,
-        setSelectedNodes,
-        selectedEdge,
-        setSelectedEdge,
-        selectedEdges,
-        setSelectedEdges,
-        isSingleEdgeSelection,
-        setIsSingleNodeSelection,
-        setIsSingleEdgeSelection,
-    };
+	return {
+		selectedNode,
+		setSelectedNode,
+		selectedNodes,
+		setSelectedNodes,
+		selectedEdge,
+		setSelectedEdge,
+		selectedEdges,
+		setSelectedEdges,
+		isSingleEdgeSelection,
+		setIsSingleNodeSelection,
+		setIsSingleEdgeSelection,
+	};
 }
