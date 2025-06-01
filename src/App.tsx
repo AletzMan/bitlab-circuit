fbe1a73910a90358c0621d7b8c9dc56f4971c97a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-05-31 18:27:09 -0600

    feat: Integrate HistoryProvider for managing history state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import "./App.css";
 import { BoardFlow } from "@/components/BoardFlow/BoardFlow";
 import { ConfigProvider } from "antd";
 import theme from "./theme/themeConfig";
+import { HistoryProvider } from "./contexts/HistoryContext";
 
 function App() {
 	return (
 		<ConfigProvider theme={theme}>
 			<ReactFlowProvider>
-				<BoardFlow />
+				<HistoryProvider>
+					<BoardFlow />
+				</HistoryProvider>
 			</ReactFlowProvider>
 		</ConfigProvider>
 	);
