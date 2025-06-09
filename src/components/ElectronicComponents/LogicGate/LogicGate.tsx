ba3970cc0a0d9b12b3dd8a13458ca78a0572df95
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-09 11:33:14 -0600

    style: refactor connection handling in LogicGate component for improved state management

--- src/components/ElectronicComponents/LogicGate/LogicGate.tsx
+++ src/components/ElectronicComponents/LogicGate/LogicGate.tsx
@@ -1,5 +1,11 @@
 import { Connection, NodeProps, Position, useNodeConnections, useReactFlow } from "@xyflow/react";
-import { AnalogNode, ComponentCollapsed, ComponentType, ITerminalSetting } from "@/types";
+import {
+	AnalogNode,
+	ComponentCollapsed,
+	ComponentType,
+	IConnectedHandles,
+	ITerminalSetting,
+} from "@/types";
 import { LockIcon, UnlockIcon } from "@/icons";
 import styles from "./styles.module.css";
 import { Terminal } from "@/components/Terminal/Terminal";
@@ -23,10 +29,14 @@ export function LogicGate({
 	parentId,
 }: NodeProps<AnalogNode>) {
 	const { updateNode } = useReactFlow();
-	const [isConnected, setIsConnected] = useState<boolean[]>([false, false]);
+	const [connectedHandlesInternal, setConnectedHandlesInternal] = useState<IConnectedHandles[]>([
+		{ isConnected: false, type: "passive" },
+		{ isConnected: false, type: "passive" },
+		{ isConnected: false, type: "passive" },
+	]);
 
 	useEffect(() => {
-		setIsConnected(connectedHandles.map((handle) => handle.isConnected));
+		setConnectedHandlesInternal(connectedHandles);
 	}, [connectedHandles]);
 
 	const isAdditionValid = collapsed === ComponentCollapsed.Add;
@@ -43,37 +53,29 @@ export function LogicGate({
 	useNodeConnections({ onConnect, onDisconnect });
 
 	const setConnectionsTerminals = (connections: Connection[], isOnConnect: boolean) => {
-		connections.map((connection) => {
-			const newState = [...isConnected];
+		connections.forEach((connection) => {
+			const newState = [...connectedHandlesInternal];
 			if (connection.target === id) {
 				const handleNumber = Number(connection.targetHandle) - 1;
-				newState[handleNumber] = isOnConnect;
-				setIsConnected(newState);
+				newState[handleNumber] = { isConnected: isOnConnect, type: "passive" };
+				setConnectedHandlesInternal(newState);
 				updateNode(id, (prevNode) => ({
 					data: {
 						...prevNode.data,
-						connectedHandles: {
-							...(prevNode as AnalogNode).data.connectedHandles,
-							[handleNumber]: isOnConnect,
-						},
+						connectedHandles: [...newState],
 					},
 				}));
-				return connection.target === id;
 			}
 			if (connection.source === id) {
 				const handleNumber = Number(connection.sourceHandle) - 1;
-				newState[handleNumber] = isOnConnect;
-				setIsConnected(newState);
+				newState[handleNumber] = { isConnected: isOnConnect, type: "passive" };
+				setConnectedHandlesInternal(newState);
 				updateNode(id, (prevNode) => ({
 					data: {
 						...prevNode.data,
-						connectedHandles: {
-							...(prevNode as AnalogNode).data.connectedHandles,
-							[handleNumber]: isOnConnect,
-						},
+						connectedHandles: [...newState],
 					},
 				}));
-				return connection.source === id;
 			}
 		});
 	};
@@ -251,14 +253,14 @@ export function LogicGate({
 				type="source"
 				position={terminalSettings.position[0]}
 				id="1"
-				isConnectable={!isConnected[0]}
+				isConnectable={!connectedHandlesInternal[0].isConnected}
 				style={terminalSettings.adjustment[0]}
 			/>
 			<Terminal
 				type="source"
 				position={terminalSettings.position[1]}
 				id="2"
-				isConnectable={!isConnected[1]}
+				isConnectable={!connectedHandlesInternal[1].isConnected}
 				style={terminalSettings.adjustment[1]}
 			/>
 			{type !== ComponentType.NOTGate && type !== ComponentType.BUFFERGate && (
@@ -266,7 +268,7 @@ export function LogicGate({
 					type="source"
 					position={terminalSettings.position[2]}
 					id="3"
-					isConnectable={!isConnected[2]}
+					isConnectable={!connectedHandlesInternal[2].isConnected}
 					style={terminalSettings.adjustment[2]}
 				/>
 			)}
