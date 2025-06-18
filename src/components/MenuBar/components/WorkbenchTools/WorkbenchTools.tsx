9fee770e944ae2b5b3e01e5b2da1572883e263f7
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-17 22:57:23 -0600

    style: update workbench tools to manage cursor and probe states based on simulation status

--- src/components/MenuBar/components/WorkbenchTools/WorkbenchTools.tsx
+++ src/components/MenuBar/components/WorkbenchTools/WorkbenchTools.tsx
@@ -1,25 +1,43 @@
 import { CursorIcon, ProbeIcon } from "@/icons";
 import { Button, Tooltip } from "antd";
 import styles from "./styles.module.css";
-import { useWorkbenchTools } from "@/store";
+import { useSimulation, useWorkbenchTools } from "@/store";
+import { useEffect } from "react";
 
 export function WorkbenchTools() {
 	const workbenchTools = useWorkbenchTools((state) => state.workbenchTools);
 	const setWorkbenchTools = useWorkbenchTools((state) => state.setWorkbenchTools);
+	const isSimulationRunning = useSimulation((state) => state.isSimulationRunning);
+
+	useEffect(() => {
+		if (isSimulationRunning) {
+			document.body.classList.remove("cursor");
+			document.body.classList.add("probe");
+			setWorkbenchTools("probe");
+		} else {
+			document.body.classList.remove("probe");
+			document.body.classList.add("cursor");
+			setWorkbenchTools("cursor");
+		}
+	}, [isSimulationRunning]);
 	return (
-		<div className={styles.workbenchTools}>
-			<Tooltip title="Cursor">
+		<div
+			className={`${styles.workbenchTools} ${
+				isSimulationRunning ? styles.workbenchTool_active : styles.workbenchTool_inactive
+			}`}
+		>
+			<Tooltip title={!isSimulationRunning ? "Cursor" : "Simulation is running"}>
 				<Button
 					type={workbenchTools === "cursor" ? "primary" : "text"}
 					icon={<CursorIcon />}
-					onClick={() => setWorkbenchTools("cursor")}
+					disabled={isSimulationRunning}
 				/>
 			</Tooltip>
-			<Tooltip title="Probe">
+			<Tooltip title={isSimulationRunning ? "Probe" : "Simulation not running"}>
 				<Button
 					type={workbenchTools === "probe" ? "primary" : "text"}
 					icon={<ProbeIcon />}
-					onClick={() => setWorkbenchTools("probe")}
+					disabled={!isSimulationRunning}
 				/>
 			</Tooltip>
 		</div>
