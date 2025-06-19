f752725c65a5a862273746553aec3974f639a399
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-19 17:51:28 -0600

    style: refactor App component to use BrowserRouter and update background color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { ReactFlowProvider } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import "./App.css";
-import { BoardFlow } from "@/components/BoardFlow/BoardFlow";
 import { ConfigProvider } from "antd";
 import theme from "./theme/themeConfig";
 import { HistoryProvider } from "./contexts/HistoryContext";
+import { BrowserRouter } from "react-router";
+import { Router } from "./pages/router";
 
 function App() {
 	return (
-		<ConfigProvider theme={theme}>
-			<ReactFlowProvider>
-				<HistoryProvider>
-					<BoardFlow />
-				</HistoryProvider>
-			</ReactFlowProvider>
-		</ConfigProvider>
+		<BrowserRouter>
+			<ConfigProvider theme={theme}>
+				<ReactFlowProvider>
+					<HistoryProvider>
+						<Router />
+					</HistoryProvider>
+				</ReactFlowProvider>
+			</ConfigProvider>
+		</BrowserRouter>
 	);
 }
 
