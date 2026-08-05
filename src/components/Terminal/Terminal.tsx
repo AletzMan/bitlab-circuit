4336e15ea897d5cd2fa882f9a005d805614fa989
BitCoder <alejo_2986@hotmail.com>
2026-08-04 18:58:20 -0600

    refactor: mejorar visualización de terminales con SVG

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -8,6 +8,10 @@ export function Terminal(props: HandleProps) {
 			{...props}
 			className={`${props.isConnectable ? styles.terminal_connectable : styles.terminal} `}
 			 
-		/>
+		>
+			<svg viewBox="0 0 33 33">
+				<line x1="0" y1="4.95" x2="33" y2="4.95"  strokeWidth="5" 	shape-rendering="crispEdges"/>
+			</svg>
+		</Handle>
 	);
 }
